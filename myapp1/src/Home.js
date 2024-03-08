f12d4ea318699ff38195746d2401a0aa6a9ef7ba
CoderXSaurabh <patilsaurabh5122001@gmail.com>
2024-03-08 21:05:07 +0530

    updated index.js file...

---
+++ myapp1/src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Home extends Component{
+    render(){
+        return (
+            <h1>Class based Component</h1>
+        )
+    }
+      
+}
+export default Home;
--- myapp1/src/index.js
+++ myapp1/src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { ReactDOM } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>Hello from index.jsx</h1>
-    <App />
-  </React.StrictMode>
-);
+let element = React.createElement("h1",
+{id:"head"},"HELLO WORLD",
+React.createElement("h2",{className:"head"},"Heading two"),
+React.createElement("button",null)
+)
+ReactDOM.render(element,document.getElementById("root"))
+
+
+
+
+// Date 07/03/2024
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// // import './index.css';
+// import App from './App';
+// import Home from './Home';
+// // import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <h1>Hello from index.jsx</h1>
+//     <App />
+//     <Home/>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
