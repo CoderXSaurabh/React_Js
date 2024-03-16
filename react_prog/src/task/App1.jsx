887aad16d4e6c92a19fcc0a8afa96df2dc36e5b2
CoderXSaurabh <patilsaurabh5122001@gmail.com>
2024-03-16 11:35:23 +0530

    Modified App.css,App1.jsx and Cards.jsx

--- react_prog/src/task/App1.jsx
+++ react_prog/src/task/App1.jsx
@@ -4,7 +4,8 @@ import { Cards } from './Cards'
 export const App1 = () => {
   return (
     <>
-        <Cards/>
+        <Cards cardcat="Netflix Original series" add="https://www.youtube.com/watch?v=ndl1W4ltcmg&t=1s" img="https://tse2.mm.bing.net/th?id=OIP.x91JfQLmVxNHTbXE7lsdqgHaNK&pid=Api&P=0&h=180"/>
+        <Cards cardcat="Netflix Original series" add="https://www.youtube.com/watch?v=ndl1W4ltcmg&t=1s" img="https://tse2.mm.bing.net/th?id=OIP.x91JfQLmVxNHTbXE7lsdqgHaNK&pid=Api&P=0&h=180"/>
     </>
   )
 }
--- react_prog/src/task/Cards.jsx
+++ react_prog/src/task/Cards.jsx
@@ -5,12 +5,12 @@ export const Cards = (props) => {
   return (
         <div className='cards'>
             <div className="card">
-                <img src="" alt="" />
+                <img src={props.img} alt="" />
                 { <div className="card-body">
                     <span className='card-categary'>{props.cardcat}</span>
                     <h3 className='title'>{props.title}</h3>
                     <a href={props.add}>
-                        <button>Wathch</button>
+                        <button>Watch</button>
                     </a>
                 </div> }
             </div>
